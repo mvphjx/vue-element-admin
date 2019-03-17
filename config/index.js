e34415529fa83ce5890ae526a3650cc223e0c170
hanjianxiang <511572653@qq.com>
2019-03-17 22:48:58 +0800

    调试模式  查看源码

--- config/index.js
+++ config/index.js
@@ -42,7 +42,7 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: true
   },
 
   build: {
@@ -60,7 +60,7 @@ module.exports = {
      * then assetsPublicPath should be set to "/bar/".
      * In most cases please use '/' !!!
      */
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
