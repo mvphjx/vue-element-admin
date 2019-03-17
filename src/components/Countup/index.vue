41dce3b1362cf07fdb0abd5df32ba05d73d90b04
hanjianxiang <511572653@qq.com>
2019-03-17 23:04:40 +0800

    组件练习

---
+++ src/components/Countup/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <span ref="countup" />
+</template>
+
+<script>
+import { CountUp } from './countUp.js'
+export default {
+  name: 'CountupDemo',
+  props: {
+    end: {
+      type: Number,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: function name(params) {
+
+      },
+      required: false
+    }
+  },
+  data() {
+    return {
+      numAnim: null
+    }
+  },
+  mounted() {
+    this.initCountUp()
+  },
+  methods: {
+    initCountUp() {
+      this.numAnim = new CountUp(this.$refs.countup,
+        this.end,
+        this.options)
+      this.numAnim.start()
+    }
+  }
+}
+</script>
