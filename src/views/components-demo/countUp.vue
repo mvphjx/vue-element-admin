2357ce49ddef1706019db217212efb9d86bb6d04
hanjianxiang <511572653@qq.com>
2019-03-17 23:12:58 +0800

    调试模式  查看源码

---
+++ src/views/components-demo/countUp.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div>
+    <div>
+      <countTo ref="child" :start="start" :end="end" :duration="3000" />
+    </div>
+    <div>
+      <el-button type="primary" @click="reStart">再来一次</el-button>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+import countTo from '@/components/CountUp'
+export default {
+  components: { countTo },
+  data() {
+    return {
+      start: 0,
+      end: 2019
+    }
+  },
+  methods: {
+    reStart() {
+      console.log('this.start', this.start)
+      this.start = 0
+      console.log(countTo)
+      console.log(this.$refs.child)
+      this.$refs.child.initCountUp()
+      // countTo.initCountUp()
+    }
+  }
+}
+</script>
